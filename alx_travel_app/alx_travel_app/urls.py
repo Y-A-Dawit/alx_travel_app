dba8ec16ad5db18d2d4edd994403ea01630902d1
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-09-07 23:24:10 +0300

    Deploy: Render + Celery + Swagger setup

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -24,7 +24,7 @@ schema_view = get_schema_view(
    openapi.Info(
       title="ALX Travel API",
       default_version='v1',
-      description="Travel Listing API",
+      description="API DOCUMENTATION",
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
--- alx_travel_app/listings/tasks.py
+++ alx_travel_app/listings/tasks.py
@@ -1,8 +1,14 @@
 # listings/tasks.py
 
 from celery import shared_task
+import time
 
 @shared_task
 def sample_task():
     print("This is a test task from Celery")
     return "Task complete"
+
+@shared_task
+def demo_sleep(seconds=3):
+    time.sleep(seconds)
+    return f"Slept for {seconds}"
